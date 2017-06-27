1e1b5da86bc7bfe2b8294946e790d63c50faa63f
martin.deluca <martin.deluca@jamgo.es>
2017-06-28 00:41:10 +0200

    UPDATE: snapshot from Workbench.  - Settings moved to options menu  - Default content loaded from resoruce file  - In case of malformed url, the default content will be loaded  - In case of loadUrl error, the default content will be loaded

--- ereuseAndroidApp/app/src/main/java/org/ereuse/scanner/activities/WorkbenchActivity.java
+++ ereuseAndroidApp/app/src/main/java/org/ereuse/scanner/activities/WorkbenchActivity.java
@@ -11,24 +11,28 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.SubMenu;
-import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.JavascriptInterface;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.barcode.Barcode;
-import com.google.zxing.common.StringUtils;
 
 import org.ereuse.scanner.R;
-import org.ereuse.scanner.data.User;
 import org.ereuse.scanner.services.api.ApiServicesImpl;
 import org.ereuse.scanner.utils.ScanUtils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 
 /**
@@ -39,7 +43,7 @@ public class WorkbenchActivity extends ScanActivity {
     private SubMenu selectDb;
     ArrayList<String> databases;
     WebView scanWebView;
-    private EditText workbenchServerAddressEditText;
+    private String workbenchServerAddress;
     private String htmlFieldId;
     private boolean urlField;
 
@@ -54,35 +58,39 @@ public class WorkbenchActivity extends ScanActivity {
         setContentView(R.layout.activity_snapshot_workbench);
         setToolbar();
 
-        this.workbenchServerAddressEditText = new EditText(this);
-        this.workbenchServerAddressEditText.setText(getWorkbenchServer());
+        this.workbenchServerAddress = getWorkbenchServer();
 
         this.scanWebView = (WebView)this.findViewById(R.id.workbench_webview);
 
         this.scanWebView.getSettings().setJavaScriptEnabled(true);
         this.scanWebView.addJavascriptInterface(new WebViewJavaScriptInterface(this), "app");
         this.scanWebView.getSettings().setJavaScriptEnabled(true);
-
+        this.scanWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(WorkbenchActivity.this, "ERROR retrieving workbench information" , Toast.LENGTH_LONG).show();
+                showDefaultWebView();
+            }
+            @Override
+            public void onReceivedHttpError(
+                WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
+                Toast.makeText(WorkbenchActivity.this, "ERROR retrieving workbench information" , Toast.LENGTH_LONG).show();
+                showDefaultWebView();
+            }
+        });
         reloadWebView();
     }
 
-    private void reloadWebView() {
-        if(this.workbenchServerAddressEditText.getText().toString().equals("")) {
-            String dynamicHtml = "<html>" +
-                    "<div>" +
-                    "<p>Clica per escanejar:</p>" +
-                    "</div>" +
-                    "<form>" +
-                    "<p>common field:</p>" +
-                    "<input type='button' value='scan' onclick='app.startJSScan(\"scanResultField\", false)' />" +
-                    "<input type='text' id='scanResultField' />" +
-                    "<p>System id:</p>" +
-                    "<input type='button' value='scan' onclick='app.startJSScan(\"systemIdField\", true)' />" +
-                    "<input type='text' id='systemIdField' />" +
-                    "</form>" +
-                    "</html>";
-            this.scanWebView.loadData(dynamicHtml,"text/html","UTF-8");
 
+
+    private void showDefaultWebView() {
+        String dynamicHtml = getDefaultWorkbenchWebViewContent();
+        this.scanWebView.loadData(dynamicHtml,"text/html","UTF-8");
+    }
+
+    private void reloadWebView() {
+        if(this.workbenchServerAddress.equals("")) {
+          showDefaultWebView();
         } else {
             final Activity activity = this;
 
@@ -91,11 +99,34 @@ public class WorkbenchActivity extends ScanActivity {
                     Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
                 }
             });
-            this.scanWebView.loadUrl(this.workbenchServerAddressEditText.getText().toString());
+
+            try { //check valid url
+                final URLConnection connection = new URL(this.workbenchServerAddress).openConnection();
+                this.scanWebView.loadUrl(this.workbenchServerAddress);
+            } catch (Exception e) {
+                showDefaultWebView();
+            }
 
         }
     }
 
+    private String getDefaultWorkbenchWebViewContent() {
+        String result = null;
+        InputStream defaultWorkbecnhContentInputStream = getResources().openRawResource(R.raw.workbench_default_webview);
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(defaultWorkbecnhContentInputStream));
+            StringBuilder out = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                out.append(line);
+            }
+            result = out.toString();
+            reader.close();
+        } catch (IOException e) {
+        }
+            return result;
+
+    }
 
     @Override
     protected void onResume()
@@ -105,18 +136,21 @@ public class WorkbenchActivity extends ScanActivity {
 
     private String getWorkbenchServer() {
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME,MODE_PRIVATE);
-        return sharedPreferences.getString("workbenchServerAddress","");
+        return sharedPreferences.getString("workbenchServerAddress", getString(R.string.workbench_default_server_address_value));
     }
 
     private void setWorkbenchServer(String server) {
-        this.workbenchServerAddressEditText.setText(server);
+        this.workbenchServerAddress = server;
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME,MODE_PRIVATE);
         sharedPreferences.edit().putString("workbenchServerAddress", server).commit();
         this.reloadWebView();
     }
 
-    public void showWorkbenchSettings(View view) {
+    public void showSettingsDialog() {
         AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        final EditText workbenchServerAddressEditText = new EditText(this);
+        workbenchServerAddressEditText.setText(workbenchServerAddress);
+
         alertBuilder.setTitle(getString(R.string.workbench_server_address))
                 .setView(workbenchServerAddressEditText)
                 .setPositiveButton(R.string.dialog_ack, new DialogInterface.OnClickListener() {
@@ -127,12 +161,13 @@ public class WorkbenchActivity extends ScanActivity {
                         dialog.dismiss();
                     }
                 })
-//                .setNeutralButton(R.string.dialog_reset_default, new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//                        setWorkbenchServer(getString(R.string.default_workbench_server_address));
-//                    }
-//                })
+                .setNeutralButton(R.string.dialog_reset_default, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        setWorkbenchServer(getString(R.string.workbench_default_server_address_value));
+                        dialog.dismiss();
+                    }
+                })
                 .setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -146,8 +181,20 @@ public class WorkbenchActivity extends ScanActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
+        inflater.inflate(R.menu.workbench_main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                showSettingsDialog();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void showLogoutDialog(){

e1118c841cca0425825c9af23ed6f6b7f99ce7f1
Xavier Bustamante Talavera <xavier@bustawin.com>
2018-06-15 11:24:03 +0200

    Set version to 1.7.0

--- ereuseAndroidApp/app/src/main/java/org/ereuse/scanner/activities/SnapshotActivity.java
+++ ereuseAndroidApp/app/src/main/java/org/ereuse/scanner/activities/SnapshotActivity.java
@@ -45,7 +45,10 @@ public class SnapshotActivity extends ScanActivity {
         Map<String, List<String>> deviceTypesMap = new HashMap<String, List<String>>();
         deviceTypesMap.put("Computer", new ArrayList<String>(Arrays.asList("Netbook", "Laptop", "Microtower", "Server")));
         deviceTypesMap.put("ComputerMonitor", new ArrayList<String>(Arrays.asList("TFT", "LCD", "OLED", "LED")));
-        deviceTypesMap.put("Peripheral", new ArrayList<String>(Arrays.asList("Scanner", "MultifunctionPrinter", "SAI", "Switch", "HUB", "Router", "Mouse", "Keyboard", "Printer")));
+        deviceTypesMap.put("Peripheral", new ArrayList<String>(Arrays.asList("Router", "Switch", "Printer",
+                "Scanner", "MultifunctionPrinter", "Terminal", "HUB", "SAI",
+                "Keyboard", "Mouse", "WirelessAccessPoint", "LabelPrinter", "Projector", "VideoconferenceDevice",
+                "WirelessMicrophone", "Scaler", "VideoScaler", "MemoryCardReader", "Amplifier", "AudioAmplifier")));
         DEVICETYPES = Collections.unmodifiableMap(deviceTypesMap);
     }
 
